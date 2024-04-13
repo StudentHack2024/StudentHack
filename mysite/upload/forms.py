b1774a0005674173f714338ad92b79abade9f5d1
e38508gv <gregor.vincent@student.manchester.ac.uk>
2024-04-13 18:06:59 +0100

    Upload image working

--- mysite/upload/forms.py
+++ mysite/upload/forms.py
@@ -3,3 +3,5 @@ from django import forms
 class image_input_form(forms.Form):
     image = forms.ImageField(label='Select a file')
 
+class email_input_form(forms.Form):
+    email = forms.EmailField(label='Enter your email')
