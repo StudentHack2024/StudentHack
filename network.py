7cc0eb4d07dbdc3bcd3b24ce189040c999435edb
u05730dg <daniel.groves@student.manchester.ac.uk>
2024-04-13 14:21:40 +0100

    For network load image paths and classify basic template

--- network.py
+++ network.py
@@ -2,14 +2,19 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+from PIL import Image
+
+def load_image(image_path):
+    return np.array(Image.open(image_path))
+
 
 # Read in data
 test_data = pd.read_csv('test.csv')
 train_data = pd.read_csv('train.csv')
 
 # Split data into x and y
-x_train, y_train = np.array(train_data['Image']), np.array(train_data['Classification'])
-x_test, y_test = np.array(test_data['Image']), np.array(test_data['Classification'])
+x_train, y_train = np.array([load_image(img) for img in train_data['Image']]), np.array(train_data['Classification'])
+x_test, y_test = np.array([load_image(img) for img in test_data['Image']]), np.array(test_data['Classification'])
 
 #Normalize data
 x_train = tf.keras.utils.normalize(x_train, axis=1)
