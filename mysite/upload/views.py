41af2637ce5fda53cf21149a91adecb5f78461a2
e38508gv <gregor.vincent@student.manchester.ac.uk>
2024-04-14 00:23:08 +0100

    Added sevrices file and machine learning

--- mysite/upload/views.py
+++ mysite/upload/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import image_input_form
+from.services.classify import classify_img
 def index(request):
     if request.method == "POST":
         form = image_input_form(request.POST, request.FILES)
         if form.is_valid():
-            return HttpResponse("File uploaded successfully")
+            prediction = classify_img(request.FILES['image'])
+            return HttpResponse("The galaxy is of type: " + str(prediction))
         else:
             return HttpResponse("Form is not valid")
     else:
