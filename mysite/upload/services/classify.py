a984355d7f22f282fcb5fd17d727cca156e58b74
Tev Partridge <tev.partridge@gmail.com>
2024-04-14 11:55:18 +0100

    finsish

--- mysite/upload/services/classify.py
+++ mysite/upload/services/classify.py
@@ -8,13 +8,15 @@ def classify_img(image):
     url = "upload" + static('galaxies.weights.h5')
 
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (50,50,1)))
+    model.add(tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (100,100,1)))
     model.add(tf.keras.layers.MaxPooling2D((2,2)))
     model.add(tf.keras.layers.Conv2D(64, (3,3), activation = 'relu'))
     model.add(tf.keras.layers.MaxPooling2D((2,2)))
-    model.add(tf.keras.layers.Conv2D(64, (3,3), activation = 'relu'))
+    model.add(tf.keras.layers.Conv2D(128, (3,3), activation = 'relu'))
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(64, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(128, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.5))
     model.add(tf.keras.layers.Dense(3, activation="softmax"))
     model.load_weights(url)
     file = image.file
--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import pandas as pd
 import random
 
 def load_image(image_path):
-    img = image.load_img("./images/" + image_path, target_size=(50, 50), color_mode='grayscale')
+    img = image.load_img("./images/" + image_path, target_size=(100, 100), color_mode='grayscale')
     img = img.rotate(random.randint(-180, 180))
     img = np.asarray(img)
     return img
@@ -25,18 +25,20 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 #Create model and add layers
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (50,50,1)))
+model.add(tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (100,100,1)))
 model.add(tf.keras.layers.MaxPooling2D((2,2)))
 model.add(tf.keras.layers.Conv2D(64, (3,3), activation = 'relu'))
 model.add(tf.keras.layers.MaxPooling2D((2,2)))
-model.add(tf.keras.layers.Conv2D(64, (3,3), activation = 'relu'))
+model.add(tf.keras.layers.Conv2D(128, (3,3), activation = 'relu'))
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(64, activation="relu"))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(3, activation="softmax"))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=11)
+model.fit(x_train, y_train, epochs=5)
 
 model.save_weights('galaxies.weights.h5')
 
